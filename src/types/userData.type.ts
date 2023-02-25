0ed15bd127eebf2c32980605e86404ef08385ad7
melkam5 <melkamargaw5@gmail.com>
2023-02-26 00:27:42 +0300

    interface fixed

--- src/types/userData.type.ts
+++ src/types/userData.type.ts
@@ -1,16 +1,20 @@
 export interface UserDataInterface {
+    id? : string;
     tg_id :number;
-    first_name: string;
-    username: string;
-    registered_name: string;
-    lang : string;
-    phone_number: string;
-    stream: string;
-    school : string;
-    payed : number[] ;
+    first_name: string | null;
+    username: string | null;
+    registered_name: string | null;
+    lang : string | null;
+    phone_number: string | null;
+    stream: string | null;
+    school : string | null;
+    payed : number ;
     points : number ;
     credited :number ;
-}
+} 
+
+
+
 
 export interface waitingListStudentInterface {
     id: string;
