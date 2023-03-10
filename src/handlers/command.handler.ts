1a011f5854aebf8c33fc69129dc0872886218a01
melkam5 <melkamargaw5@gmail.com>
2023-03-10 17:17:32 +0300

    a refresh option for admins added

--- src/handlers/command.handler.ts
+++ src/handlers/command.handler.ts
@@ -4,6 +4,12 @@ import { lan } from "../middlewares/fechUserData.middleware";
 import { MyContext } from "../types/context.type";
 import { ObjectKey } from "../types/loc.type";
 import { mainMenuamh, mainMenu } from "../keyboards/mainmenu.custom.keyboard";
+import { PrismaClient } from "@prisma/client";
+import { bot } from "../bot";
+import { photoContact } from "../config/botData";
+import { approval } from "../utiles/functions";
+const prisma = new PrismaClient();
+
 
 const composer  = new Composer <MyContext> ();
 
@@ -14,5 +20,36 @@ composer.command('start', async (ctx)=>{
 composer.command("cancel", async (ctx) => {
     await ctx.conversation.exit();
 });
+
+
+composer.command('refresh', async (ctx)=>{
+    if (ctx.chat.id == Number(process.env.ADMIN_ONE)){
+        const pending = await prisma.waitingListStudent.findMany()
+        await pending.forEach(async(student)=>{
+            const stdu = await prisma.user.findFirst({where : {tg_id : student.stusent_tg_Id}})
+            if (stdu){
+                ctx.replyWithPhoto(student.caption || photoContact
+                    , { 
+                        caption: `Id : [ ${ctx.chat?.id} ] 
+            ➖➖➖➖➖➖➖➖➖➖
+            Name : ${stdu.registered_name}
+            UserName : ${stdu.username}
+            Phone_No : ${stdu.phone_number}
+            Bank : ${student.bank_name}
+            Stream : ${student.stream}
+            ➖➖➖➖➖➖➖➖➖➖
+            Caption : ${"NO CAPTION FOR REFRESH"}
+            ➖➖➖➖➖➖➖➖➖➖`
+                    , reply_markup : approval 
+                })
+            }
+        }
+      )
+    }
+    else {
+        await ctx.reply(loc[lan as ObjectKey].message_select_option , {reply_markup : ctx.userData.lang=='amh' ? mainMenuamh : mainMenu, parse_mode: "HTML" })
+    }
+})
+
 
 export default composer ;
