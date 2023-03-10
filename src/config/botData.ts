d22daf1fbad98dacedc25f4e4c89a79e925a9441
melkam5 <melkamargaw5@gmail.com>
2023-03-10 19:17:54 +0300

    small changes

--- src/config/botData.ts
+++ src/config/botData.ts
@@ -1,9 +1,6 @@
-///// Photos
-
 export const photoPyment ='AgACAgQAAxkBAALx0GObiUiUzrVMdazxLHgizWtY9VFVAAIPvDEbPlThUCfQyRIgTVpfAQADAgADeQADLAQ'
 export const photoContact = 'AgACAgQAAxkBAALxzmObiR6rnNtHM95KPmeXjMk__6ldAAIDvDEbPlThUJusobFWESHNAQADAgADeQADLAQ'
 export const photoInvite = 'AgACAgQAAxkBAALx0mObiVpztIGSeKtqAhZng8tPyN7zAAIQvDEbPlThUNeWS-udScKFAQADAgADeQADLAQ'
-
 export const connection_url = "mongodb+srv://Admin:OK9EDSdhwMDXzmy9@cluster0.qawncsk.mongodb.net/?retryWrites=true&w=majority"
 
 
@@ -18,17 +15,3 @@ export const referalBonus = {
     point_value : 15,
     tutor_price : 150,
 }
-
-
-///// During test
-/*
-export const bot_user_name = 'njnjnjnnjnj_bot' ;
-export const public_channel = '@hjhjhbnb' ;
-export const public_channel_link = 'https://t.me/hjhjhbnb'
-export const private_channel_na = -1001878207962 ;
-export const private_channel_so =  -1001850991435 ;
-export const admin_one = 691540144 ;
-export const admin_two = 691540144 ;
-export const messangeradmin = 691540144;
-
-*/
